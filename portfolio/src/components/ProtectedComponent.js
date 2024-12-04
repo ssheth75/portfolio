f745903257bd512854574080e110b9adf07b868c
Shirvil Sheth <96599193+ssheth75@users.noreply.github.com>
2024-12-03 19:29:47 -0500

    set home page

--- portfolio/src/components/ProtectedComponent.js
+++ portfolio/src/components/ProtectedComponent.js
@@ -33,153 +33,156 @@ function ProtectedComponent() {
 
   if (projectData) {
     return (
-      <motion.div
-        className="flex flex-col items-center border w-full max-w-5xl border-black bg-primaryColor shadow-2xl p-8 mt-20"
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6, ease: "easeOut" }}
-        viewport={{ once: true }}
-      >
-        <div className="flex flex-col text-center items-center">
-          <h2 className="font-ariata pb-2">{projectData.header}</h2>
-          <h1 className="font-ariata text-4xl w-full">{projectData.title}</h1>
-          <h1 className="font-ariata text-l w-full mt-2">
-            {projectData.technologies}
-          </h1>{" "}
-          <img className="w-11 pt-2" src="intel.png" />
-        </div>
+      <div className="mb-10">
+        <motion.div
+          className="flex flex-col items-center border w-full max-w-5xl border-black bg-primaryColor shadow-2xl p-8 mt-20"
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, ease: "easeOut" }}
+          viewport={{ once: true }}
+        >
+          <div className="flex flex-col text-center items-center">
+            <h2 className="font-ariata pb-2">{projectData.header}</h2>
+            <h1 className="font-ariata text-4xl w-full">{projectData.title}</h1>
+            <h1 className="font-ariata text-l w-full mt-2">
+              {projectData.technologies}
+            </h1>{" "}
+            <img className="w-11 pt-2" src="intel.png" />
+          </div>
 
-        <div className="flex flex-col w-full text-left font-consolas text-sm pt-8">
-          {/* Text Section */}
-          <h2 className="font-ariata text-xl">{projectData.sections[0]}</h2>
-          <hr className="border-white my-2" />
-          <h2>{projectData.purpose}</h2>
-          <h2 className="font-ariata text-xl mt-2">
-            {projectData.sections[1]}
-          </h2>
-          <hr className="border-white my-2" />
-          <h2 className="mb-2">{projectData.ts[0]}</h2>
-          <h2>{projectData.ts[1]}</h2>{" "}
-          <ul className="list-disc px-10 py-6">
-            <li className="my-2">
-              {projectData.ts[2]}
-              <br></br>
-              {projectData.ts[3]}
-            </li>
-            <li className="my-2">
-              {projectData.ts[4]}
-              <br></br>
-              {projectData.ts[5]}
-            </li>
-            <li className="my-2">
-              {projectData.ts[6]}
-              <br></br>
-              {projectData.ts[7]}
-            </li>
-          </ul>
-          <h2 className="font-ariata text-xl mt-2">
-            {projectData.sections[2]}
-          </h2>
-          <hr className="border-white my-2" />
-          <h2>
-            {projectData.adc[0]} <br></br> {projectData.adc[1]}
-          </h2>
-          <table className="min-w-full border-collapse border border-gray-300 text-sm my-4">
-            <thead className="text-base">
-              <tr>
-                <th className="border border-gray-300 px-4 py-2 text-left">
-                  {projectData.columns[0]}
-                </th>
-                <th className="border border-gray-300 px-4 py-2 text-left">
-                  {projectData.columns[1]}
-                </th>
-                <th className="border border-gray-300 px-4 py-2 text-left">
-                  {projectData.columns[2]}
-                </th>
-                <th className="border border-gray-300 px-4 py-2 text-left">
-                  {projectData.columns[3]}
-                </th>
-                <th className="border border-gray-300 px-4 py-2 text-left">
-                  {projectData.columns[4]}
-                </th>
-                <th className="border border-gray-300 px-4 py-2 text-left">
-                  {projectData.columns[5]}
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {projectData.rows.map((row) => (
-                <tr key={row.ID} className="">
-                  <td className="border border-gray-300 px-4 py-2">{row.ID}</td>
-                  <td className="border border-gray-300 px-4 py-2">
-                    {row["Chipset"]}
-                  </td>
-                  <td className="border border-gray-300 px-4 py-2">
-                    {row["Connection Type"]}
-                  </td>
-                  <td className="border border-gray-300 px-4 py-2">
-                    {row["Device Type"]}
-                  </td>
-                  <td className="border border-gray-300 px-4 py-2">
-                    {row["Manufacturer"]}
-                  </td>
-                  <td className="border border-gray-300 px-4 py-2">
-                    {row["User Rating"]}
-                  </td>
+          <div className="flex flex-col w-full text-left font-consolas text-sm pt-8">
+            {/* Text Section */}
+            <h2 className="font-ariata text-xl">{projectData.sections[0]}</h2>
+            <hr className="border-white my-2" />
+            <h2>{projectData.purpose}</h2>
+            <h2 className="font-ariata text-xl mt-2">
+              {projectData.sections[1]}
+            </h2>
+            <hr className="border-white my-2" />
+            <h2 className="mb-2">{projectData.ts[0]}</h2>
+            <h2>{projectData.ts[1]}</h2>{" "}
+            <ul className="list-disc px-10 py-6">
+              <li className="my-2">
+                {projectData.ts[2]}
+                <br></br>
+                {projectData.ts[3]}
+              </li>
+              <li className="my-2">
+                {projectData.ts[4]}
+                <br></br>
+                {projectData.ts[5]}
+              </li>
+              <li className="my-2">
+                {projectData.ts[6]}
+                <br></br>
+                {projectData.ts[7]}
+              </li>
+            </ul>
+            <h2 className="font-ariata text-xl mt-2">
+              {projectData.sections[2]}
+            </h2>
+            <hr className="border-white my-2" />
+            <h2>
+              {projectData.adc[0]} <br></br> {projectData.adc[1]}
+            </h2>
+            <table className="min-w-full border-collapse border border-gray-300 text-sm my-4">
+              <thead className="text-base">
+                <tr>
+                  <th className="border border-gray-300 px-4 py-2 text-left">
+                    {projectData.columns[0]}
+                  </th>
+                  <th className="border border-gray-300 px-4 py-2 text-left">
+                    {projectData.columns[1]}
+                  </th>
+                  <th className="border border-gray-300 px-4 py-2 text-left">
+                    {projectData.columns[2]}
+                  </th>
+                  <th className="border border-gray-300 px-4 py-2 text-left">
+                    {projectData.columns[3]}
+                  </th>
+                  <th className="border border-gray-300 px-4 py-2 text-left">
+                    {projectData.columns[4]}
+                  </th>
+                  <th className="border border-gray-300 px-4 py-2 text-left">
+                    {projectData.columns[5]}
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-          <h2 className="font-ariata text-xl mt-2">
-            {projectData.sections[3]}
-          </h2>
-          <hr className="border-white my-2" />
-          <h2>{projectData.implementation}</h2>
-          <h2 className="font-ariata text-base mt-6">
-            {projectData.sections[4]}
-          </h2>
-          <hr className="border-white my-1" />
-          <h2>{projectData.win}</h2>
-          <div className="py-4">
-            <SyntaxHighlighter language="csharp" style={oneDark}>
-              {projectData.query}
-            </SyntaxHighlighter>
+              </thead>
+              <tbody>
+                {projectData.rows.map((row) => (
+                  <tr key={row.ID} className="">
+                    <td className="border border-gray-300 px-4 py-2">
+                      {row.ID}
+                    </td>
+                    <td className="border border-gray-300 px-4 py-2">
+                      {row["Chipset"]}
+                    </td>
+                    <td className="border border-gray-300 px-4 py-2">
+                      {row["Connection Type"]}
+                    </td>
+                    <td className="border border-gray-300 px-4 py-2">
+                      {row["Device Type"]}
+                    </td>
+                    <td className="border border-gray-300 px-4 py-2">
+                      {row["Manufacturer"]}
+                    </td>
+                    <td className="border border-gray-300 px-4 py-2">
+                      {row["User Rating"]}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <h2 className="font-ariata text-xl mt-2">
+              {projectData.sections[3]}
+            </h2>
+            <hr className="border-white my-2" />
+            <h2>{projectData.implementation}</h2>
+            <h2 className="font-ariata text-base mt-6">
+              {projectData.sections[4]}
+            </h2>
+            <hr className="border-white my-1" />
+            <h2>{projectData.win}</h2>
+            <div className="py-4">
+              <SyntaxHighlighter language="csharp" style={oneDark}>
+                {projectData.query}
+              </SyntaxHighlighter>
+            </div>
+            <h2>{projectData.implementations[0]}</h2>
+            <h2 className="py-4">{projectData.implementations[1]}</h2>
+            <h2 className="py-4">{projectData.implementations[2]}</h2>
+            <h2 className="font-ariata text-xl">{projectData.sections[5]}</h2>
+            <hr className="border-white my-2" />
+            <h2>{projectData.popup[0]}</h2>
+            <h2 className="py-4">{projectData.popup[1]}</h2>
+            <div className="w-full justify-center flex py-8">
+              <img src="intelpopup.png" className="w-2/3"></img>
+            </div>
+            <h2 className="font-ariata text-xl">{projectData.sections[6]}</h2>
+            <hr className="border-white my-2" />
+            <h2 className="text-lg">{projectData.discussion[0]}</h2>
+            <h2 className="pb-4">{projectData.discussion[1]}</h2>
+            <h2 className="text-lg">{projectData.discussion[2]}</h2>
+            <ul className="list-disc px-10 py-6">
+              <li className="my-2">
+                {projectData.discussion[3]} <br></br>
+                {projectData.discussion[4]}
+              </li>
+              <li className="my-2">
+                {projectData.discussion[5]} <br></br>
+                {projectData.discussion[6]}
+              </li>
+              <li className="my-2">
+                {projectData.discussion[7]}
+                <br></br>
+                {projectData.discussion[8]}
+              </li>
+            </ul>
+            <h2 className="text-lg">{projectData.discussion[9]}</h2>
+            <h2>{projectData.discussion[10]}</h2>
           </div>
-          <h2>{projectData.implementations[0]}</h2>
-          <h2 className="py-4">{projectData.implementations[1]}</h2>
-          <h2 className="py-4">{projectData.implementations[2]}</h2>
-          <h2 className="font-ariata text-xl">{projectData.sections[5]}</h2>
-          <hr className="border-white my-2" />
-          <h2>{projectData.popup[0]}</h2>
-          <h2 className="py-4">{projectData.popup[1]}</h2>
-          <div className="w-full justify-center flex py-8">
-            <img src="intelpopup.png" className="w-2/3"></img>
-          </div>
-          <h2 className="font-ariata text-xl">{projectData.sections[6]}</h2>
-          <hr className="border-white my-2" />
-          <h2 className="text-lg">{projectData.discussion[0]}</h2>
-          <h2 className="pb-4">{projectData.discussion[1]}</h2>
-          <h2 className="text-lg">{projectData.discussion[2]}</h2>
-          <ul className="list-disc px-10 py-6">
-            <li className="my-2">
-              {projectData.discussion[3]} <br></br>
-              {projectData.discussion[4]}
-            </li>
-            <li className="my-2">
-              {projectData.discussion[5]} <br></br>
-              {projectData.discussion[6]}
-            </li>
-            <li className="my-2">
-              {projectData.discussion[7]}
-              <br></br>
-              {projectData.discussion[8]}
-              {projectData.discussion[9]}
-            </li>
-          </ul>
-          <h2 className="text-lg">{projectData.discussion[10]}</h2>
-          <h2>{projectData.discussion[11]}</h2>
-        </div>
-      </motion.div>
+        </motion.div>
+      </div>
     );
   }
 
